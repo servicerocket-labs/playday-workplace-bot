53f8c225e59974b20ff326c1f626129fa30784f2
ShilanMa <117870522+ShilanMa@users.noreply.github.com>
2024-02-23 15:33:37 +1100

    send joke when user message bot

--- index.js
+++ index.js
@@ -1,21 +1,75 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import axios from 'axios';
+import { configDotenv } from 'dotenv';
 
+configDotenv();
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World - workplace!');
+app.use(bodyParser.json());
+
+app.get('/', (_req, res) => {
+  res.send('App is live');
 });
 
 app.get('/webhook/page', (req, res) => {
   res.send(req.query['hub.challenge']);
 });
 
-app.post('/webhook/page', (req, res) => {
-  console.log('Got body:', req);
-  res.send('hello workplace');
+app.post('/webhook/page', async (req, res) => {
+  const entry = req.body.entry;
+
+  const sender_id = entry[0].messaging[0].sender.id;
+  const joke = await fetch_joke();
+
+  send_message(sender_id, joke);
+  res.status(200);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
+
+async function fetch_joke() {
+  const res = await axios.get('https://icanhazdadjoke.com/', {
+    headers: { Accept: 'application/json' },
+  });
+  return res.data.joke;
+}
+
+function send_message(recipient_id, message_text) {
+  console.log(`sending message to ${recipient_id}: ${message_text}`);
+
+  const params = {
+    access_token: process.env.WORKPLACE_TOKEN,
+  };
+
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  const data = {
+    recipient: {
+      id: recipient_id,
+    },
+    message: {
+      text: message_text,
+    },
+  };
+
+  axios
+    .post('https://graph.facebook.com/v13.0/me/messages', data, {
+      params,
+      headers,
+    })
+    .then((response) => {
+      if (response.status !== 200) {
+        console.error(response.status);
+        console.error(response.data);
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
